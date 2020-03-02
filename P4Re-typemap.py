ed0575e7271f623dc5d32e145ad9abbeb0da9c31
Jack <jknobel@me.com>
2020-03-02 17:27:35 +1100

    Fixes for python3

--- P4Re-typemap.py
+++ P4Re-typemap.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import subprocess
 
-output = subprocess.check_output("p4 typemap -o", shell=True)
+output = subprocess.check_output("p4 typemap -o", shell=True).decode()
 for typemap in output.rpartition('TypeMap:')[-1].splitlines():
     if typemap:
-        print "%s" % typemap
+        print("%s" % typemap)
         subprocess.call("p4 retype -t %s" % typemap, shell=True)
