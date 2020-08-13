012ddc57d532607287043a0f4395d3e492e5edbf
Jack Knobel <jknobel@me.com>
2020-08-13 18:37:11 +1000

    Fixed issue with Changelist not being correctly passed to command. Fixed issue with async commands not passing perforce credentials to the commands

--- P4Retrieve.py
+++ P4Retrieve.py
@@ -8,50 +8,61 @@ from concurrent import futures
 # Make sure this is running in the same directory as your P4Client (so 'p4 where' works correctly)
 # Easiest way to do this is via right clicking on your workspace root in p4v and selecting "Open command window here"
 
-P4_CONTEXT = ""
+class Perforce:
+    # Target CL
+    Changelist = 0
 
-def RunP4CommandWithResult(command):
-    output = []
-    #print("Running Command %s" % command)
-    from subprocess import Popen, PIPE
-    pipe = Popen( ["p4", "-G" ] + command.split(), stdout=PIPE).stdout
-    try:
-        while 1:
-            record = marshal.load( pipe )
-            output.append( record )
-    except EOFError:
-        pass
-    pipe.close()
+    # Optional Credentials
+    CredentialString = ""
 
-    if output[0][b'code'] == b'error':
-        print("P4 Command: %s failed with error: %s" % (command, output[0][b'data']))
-        output = []
-    return output
-
-def DownloadFile(p4_file):
-    print("Downloading file %s" % p4_file)
+    def __init__(self, InChangelist, InCredentials):
+        self.Changelist         = InChangelist
+        self.CredentialString   = InCredentials
         
-    # Convert to Local Path
-    local_file = RunP4CommandWithResult("%s where %s" % (P4_CONTEXT, p4_file))[0][b'path'].decode("utf-8")
 
-     # Actual download of file
-    RunP4CommandWithResult("%s print -q -o %s %s" % (P4_CONTEXT, local_file, p4_file))
+    def RunP4CommandWithResult(self, command):
+        output = []
+        from subprocess import Popen, PIPE
+        p4_command = "p4 -G %s %s" % (self.CredentialString, command)
+        #print(p4_command)
+        pipe = Popen(p4_command.split(), stdout=PIPE).stdout
+        try:
+            while 1:
+                record = marshal.load( pipe )
+                output.append( record )
+        except EOFError:
+            pass
+        pipe.close()
+        if output[0][b'code'] == b'error':
+            print("P4 Command: '%s' failed with error: [%s]" % (p4_command, output[0][b'data']))
+            output = []
+        return output
 
-    print("Finished downloading: %s" % local_file)
+    def DownloadFile(self, p4_file):
 
-def Run(Target_CL, Synchronous):
+        print("Downloading file %s" % p4_file)
+    
+        # Convert to Local Path
+        local_file = self.RunP4CommandWithResult("where %s" % p4_file)[0][b'path'].decode("utf-8")
+
+         # Actual download of file
+        self.RunP4CommandWithResult("print -q -o %s %s@=%s" % (local_file, p4_file, self.Changelist))
+
+        print("Finished downloading: %s" % local_file)
+
+def Run(P4_Object, AsyncCommands):
 
     # Get all files in the specified changelist
     print("Finding all files to download...")
-    file_info = RunP4CommandWithResult("%s files @=%s" % (P4_CONTEXT, Target_CL))
+    file_info = P4_Object.RunP4CommandWithResult("files @=%s" % P4_Object.Changelist)
 
-    if Synchronous == True:
-        for file_dict in file_info:
-            DownloadFile((file_dict[b'depotFile']).decode("utf-8"))
-    else:
+    if AsyncCommands == True:
         with futures.ProcessPoolExecutor() as pool:
             for file_dict in file_info:
-                pool.submit(DownloadFile, (file_dict[b'depotFile']).decode("utf-8"))
+                pool.submit(P4_Object.DownloadFile, file_dict[b'depotFile'].decode("utf-8"))
+    else:
+        for file_dict in file_info:
+            P4_Object.DownloadFile(file_dict[b'depotFile'].decode("utf-8"))
 
 if __name__ == '__main__':
 
@@ -60,11 +71,12 @@ if __name__ == '__main__':
                     dest="Changelist",
                     help="The changelist we want to download")
 
-    parser.add_argument("-P4", "--P4Context",
-                    dest="P4Context",
-                    help="Any context data needed to make this run (-p [P4PORT] -c [P4Client] etc")
+    parser.add_argument("-p4", "--credentials",
+                    dest="P4Creds",
+                    help="Any context data needed to make this run (-p [P4PORT] -c [P4Client] etc",
+                    default="")
 
-    parser.add_argument("-ST", "--Synchronous",
+    parser.add_argument("-st", "--Synchronous",
                     dest="Synchronous",
                     help="Forces the downloads to be sequential instead of parallel",
                     action="store_const",
@@ -72,13 +84,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if(args.P4Context != None):
-        P4_CONTEXT = args.P4Context
-
     if args.Changelist == None:
         args.Changelist = input("Enter the changelist number you want to download: ")
     
     if args.Changelist != None:
-        Run(args.Changelist, args.Synchronous)
+        p4_object = Perforce(args.Changelist, args.P4Creds)
+        Run(p4_object, not args.Synchronous)
     else:
         Exception("No changelist passed, aborting!")
